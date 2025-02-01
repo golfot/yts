b46f957b8d3b32fae811b1d78e18e274c187f858
Golfot <rizkifundie@gmail.com>
2025-02-02 06:36:22 +0700

    Update asisten.js

--- asisten.js
+++ asisten.js
@@ -70,7 +70,7 @@ export async function getRoast(img = '') {
         console.log('Processing image:', img); // Log informasi gambar
         const imageDesc = await describeImage(img);
         console.log('Image description:', imageDesc); // Log deskripsi gambar
-        const prompt = `Analisis gambar berikut dan ekstrak teks yang ada. Berikan jawaban terbaik berdasarkan konteks. Kemudian, buatkan 20 judul video Shorts yang menarik perhatian dan dapat memikat penonton dan Buatlah Singkat dan Menarik dan Bangkitkan Rasa Penasaran untuk menontonnya pakai bahasa indonesia. Setiap judul harus maksimal 55 karakter dan sertakan emotikon untuk meningkatkan daya tariknya.
+        const prompt = `Analisis gambar berikut dan ekstrak teks yang ada. Berikan judul terbaik berdasarkan konteks. Kemudian, buatkan 20 judul video Shorts yang Menarik dan Menimbulkan Rasa Penasaran danMenyesuaikan Dengan Isi Kontenpakai bahasa indonesia. Setiap judul harus maksimal 50 karakter dan sertakan emotikon untuk meningkatkan daya tariknya.
         
         ${imageDesc}
     `
